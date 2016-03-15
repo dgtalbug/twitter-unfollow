ab9eb7e956fef175525d7919de2e48e992bdaef9
kesavan <kesavan.g@cloudenablers.com>
2016-03-15 14:38:02 +0530

    without value to iter in items

--- twitter_conn.py
+++ twitter_conn.py
@@ -1,15 +1,15 @@
 import tweepy
 import time
 from datetime import datetime
-fix_date = datetime.strptime("2014-11-05 05:58:53", "%Y-%m-%d %H:%M:%S")
+fix_date = datetime.strptime("2014-11-05 05:58:53", "%Y-%m-%d %H:%M:%S") # friends added after this date will be removed
 
 
-auth = tweepy.OAuthHandler("xxxxxxxx",
-                           "yyyyyyyyyy")
+auth = tweepy.OAuthHandler("xxxxxxxxxxxxxxxxx", 
+                           "yyyyyyyyyyyyyyyyyyyyyyyyyyyyy") #consumer api key, consumer secret key 
 
 
-auth.set_access_token("000000000-zzzzzzzzzzzzzz",
-                      "dfadasssssssssssssss")
+auth.set_access_token("zzzzzzzzzzzzzzzzzzzzzzzzzzz",
+                      "ssssssssssssssssssssssssssss")  # access token, access token secret
 
 client = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 wait = 0
@@ -19,15 +19,16 @@ print user.name
 
 while True:
     try:
-        for friend in tweepy.Cursor(client.friends).items(2942):
+        for friend in tweepy.Cursor(client.friends).items():
             if friend.created_at > fix_date:
-                print "reaching"
+                print ">>>>>>>>>>>>>>>>>>>>>>>>"
                 print friend.name
                 print friend.created_at
+                print "<<<<<<<<<<<<<<<<<<<<<<<<"
                 client.destroy_friendship(friend.id)
                 wait += 1
                 if wait % 5 ==0:
-                    print "waiting"
+                    print "waiting for 60sec"
                     time.sleep(60)
         break
     except tweepy.TweepError, e:
