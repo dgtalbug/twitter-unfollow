5c822ff93f046b2882ee6172eff5063c1f49fa2b
kesavan <kesavan.g@cloudenablers.com>
2016-03-15 13:01:25 +0530

    With rate limit notification

--- twitter_conn.py
+++ twitter_conn.py
@@ -4,23 +4,24 @@ from datetime import datetime
 fix_date = datetime.strptime("2014-11-05 05:58:53", "%Y-%m-%d %H:%M:%S")
 
 
-auth = tweepy.OAuthHandler("p34sYvWiq22v7fQPiv8FjlU8r",
-                           "aYHbQOcO7q8m1sKv6ACGY078K1DBob6onvIlKElMIkdYSf4MCg")
+auth = tweepy.OAuthHandler("xxxxxxxx",
+                           "yyyyyyyyyy")
 
-auth.set_access_token("317918338-oOHo9Y1AZ2HJWYNnCZ89N7lszbZxVv1M0n77FOcE",
-                      "ztKIEukoEaxk2ezrc5pb5aUUmWg13bqE8F9kQ9asfUu32")
 
-client = tweepy.API(auth, wait_on_rate_limit=True)
+auth.set_access_token("000000000-zzzzzzzzzzzzzz",
+                      "dfadasssssssssssssss")
+
+client = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 wait = 0
 
 user = client.me()
 print user.name
 
-friends = client.friends()
 while True:
     try:
-        for friend in tweepy.Cursor(client.friends).items():
+        for friend in tweepy.Cursor(client.friends).items(2942):
             if friend.created_at > fix_date:
+                print "reaching"
                 print friend.name
                 print friend.created_at
                 client.destroy_friendship(friend.id)
